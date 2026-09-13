bfe3b2c40e529bacffd60f9052bf2e64efd2eb4e
Aissata-Sow788 <saissata676@gmail.com>
2026-09-13 01:31:37 +0000

    feat: page accueil

---
+++ src/app/features/home-citoyen/home-citoyen.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-home-citoyen',
+  styleUrl: './home-citoyen.css',
+  templateUrl: './home-citoyen.html',
+})
+export class HomeCitoyen {}
