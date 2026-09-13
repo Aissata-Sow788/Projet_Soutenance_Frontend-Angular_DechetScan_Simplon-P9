4b40f2af3d9523a870bd474d8c109cfeb759e740
Aissata-Sow788 <saissata676@gmail.com>
2026-09-13 01:29:47 +0000

    chore: dependances (Leaflet, Tailwind), config PWA/HTTP, routes globales et assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,36 @@
 import { Routes } from '@angular/router';
 import { Accueil } from './features/accueil/accueil';
+import { Inscription } from './features/authentification/inscription/inscription';
+import { Connexion } from './features/authentification/connexion/connexion';
+import { Scan } from './features/scan/scan';
+import { AnalyseIA } from './features/analyse-ia/analyse-ia';
+import { PointsCollecte } from './features/points-collecte/points-collecte';
+import { HomeCitoyen } from './features/home-citoyen/home-citoyen';
+import { DetailPointCollecte } from './features/detail-point-collecte/detail-point-collecte';
 
 export const routes: Routes = [
   {
     path: '', component: Accueil
-  }
+  },
+  {
+    path: 'inscription', component: Inscription
+  },
+  {
+    path: 'connexion', component: Connexion
+  },
+  {
+    path: 'scan', component: Scan
+  },
+  {
+    path: 'analyse-ia', component: AnalyseIA
+  },
+  {
+    path: 'points-collecte', component: PointsCollecte
+  },
+  {
+  path: 'points-collecte/:id', component: DetailPointCollecte
+  },
+  {
+    path: 'home-citoyen', component: HomeCitoyen
+  },
 ];
