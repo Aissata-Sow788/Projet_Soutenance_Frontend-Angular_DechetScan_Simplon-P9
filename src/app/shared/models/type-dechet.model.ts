5b38a5418fa25b4ab64bf4ba86c67cff186dba12
Aissata-Sow788 <saissata676@gmail.com>
2026-09-16 15:31:30 +0000

    feat: consommation API du référentiel des déchets

--- src/app/shared/models/type-dechet.model.ts
+++ src/app/shared/models/type-dechet.model.ts
@@ -1,6 +1,18 @@
-// Type de déchet du référentiel (plastique, verre, métal...)
+import { ConseilTri } from "./conseil-tri.model";
+
+
+// Représente un type de déchet du référentiel.
 export interface TypeDechet {
-  idType: number;
+
+  // Identifiant du type de déchet.
+  idTypeDechet: number;
+
+  // Nom du type de déchet.
   nom: string;
+
+  // Description du type de déchet.
   description: string;
+
+  // Conseil de tri associé au type.
+  conseil: ConseilTri | null;
 }
